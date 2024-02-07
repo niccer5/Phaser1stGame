ebbd7e3919def37030400e7a952041f767c7e67e
niccer5 <158280057+niccer5@users.noreply.github.com>
2024-02-07 14:49:57 +0200

    додали анімацію і фізику спрайту

--- game.js
+++ game.js
@@ -16,6 +16,7 @@ var config = {
     }
 };
 
+var player;
 var platforms;
 
 var game = new Phaser.Game(config);
@@ -29,8 +30,6 @@ function preload ()
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
-var platforms;
-
 function create ()
 {
     this.add.image(400, 300, 'sky');
@@ -42,6 +41,31 @@ function create ()
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
+
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
 }
 
 function update ()
