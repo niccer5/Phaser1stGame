ebc03a2186124f6fcc505a6995539ae12e3e3d0b
niccer5 <158280057+niccer5@users.noreply.github.com>
2024-03-18 13:34:18 +0200

    Update game.js

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 },
+            gravity: { y: 250 },
             debug: false
         }
     },
